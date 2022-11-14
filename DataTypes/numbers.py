25c5e1310c6ccc646285bfe4d463ac7c0c0c63c0
Kelvin Maina <mainakelvin360@gmail.com>
2022-11-15 02:52:12 +0300

    Data types

---
+++ DataTypes/numbers.py
@@ -0,0 +1,16 @@
+#intergers
+positiveInteger=30
+negativeInteger=-20
+#float
+floatNumber=3.5
+floatNegative=-9.1
+#scientific numbers
+standanrdForm=3e4
+#boolean
+trueBoolean=True
+falseBoolean=False
+#complex numbers
+complexNumber1=3+10j
+complexNumber2=6-10j
+
+print(complexNumber1*complexNumber2)
